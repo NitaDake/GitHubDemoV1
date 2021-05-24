e3de8ab7e8e9bb8e1645f0153e281e8ffea5853b
vaish <vaishukhairnar30@gmail.com>
2021-05-24 14:34:05 +0530

    First Committed

---
+++ src/com/csi/gitdemo/TestT.java
@@ -0,0 +1,12 @@
+package com.csi.gitdemo;
+
+public class TestT {
+public static void main(String[] args) {
+	try{
+		System.out.println("try");
+	}
+	finally{
+		System.out.println("catch");
+	}
+}
+}
